148fec02fc98fde37bc3736c4970fad68f116775
Wandile Shabangu <wandile.shabangu@dlalagroup.com>
2026-03-09 21:27:20 +0200

    UI design touch ups
    Former-commit-id: c1e46727ac9c0f6112772ead8dd9256019fe6d3c

--- vitra-fruit-react/src/components/Contact.js
+++ vitra-fruit-react/src/components/Contact.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
 function Contact() {
+  const galleryImages = [1, 2, 3, 4, 5].map((imageNumber) => ({
+    src: `/images/picture${imageNumber}.jpeg`,
+    alt: `Vitra Fruit gallery image ${imageNumber}`
+  }));
+
   return (
     <section className="contact" id="contact">
       <div className="container">
-        <a href="/contact.html" className="btn btn-contact-page">Contact Us</a>
+        <div className="contact-gallery" aria-label="Contact gallery">
+          {galleryImages.map((image) => (
+            <figure key={image.src} className="contact-gallery-item">
+              <img src={image.src} alt={image.alt} loading="lazy" decoding="async" />
+            </figure>
+          ))}
+          <a href="/contact.html" className="btn btn-contact-page">Contact Us</a>
+        </div>
       </div>
     </section>
   );
